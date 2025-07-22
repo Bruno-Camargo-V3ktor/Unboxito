1c325e7f3b73c7976ed70d1cf7c72219b9384659
Bruno-Camargo-V3ktor <v3ktoryt@gmail.com>
2025-07-22 18:22:20 -0300

    feat: add Value type and Numbers type create

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,16 @@
+use std::collections::HashMap;
 
+pub enum Value {
+    Null,
+    Boolean(bool),
+    Number(Numbers),
+    Str(String),
+    List(Vec<Value>),
+    Object(HashMap<String, Value>),
+    Bytes(Vec<u8>),
+}
+
+pub enum Numbers {
+    Int(i64),
+    Float(f64),
+}
