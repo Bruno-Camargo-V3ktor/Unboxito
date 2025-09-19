8f283104773c8451c8db33a869478fcb49c40b17
Bruno-Camargo-V3ktor <v3ktoryt@gmail.com>
2025-09-19 14:15:12 -0300

    feat: create derive macros project

---
+++ crates/unboxit-derive/src/lib.rs
@@ -0,0 +1,12 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{DeriveInput, parse_macro_input};
+
+#[proc_macro_derive(UnboxitSerialize)]
+pub fn unboxit_serialize_derive(input: TokenStream) -> TokenStream {
+    let ast = parse_macro_input!(input as DeriveInput);
+
+    let g = quote! {};
+
+    g.into()
+}
